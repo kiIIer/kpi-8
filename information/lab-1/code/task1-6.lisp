f04e061e31f6d895e117aaea08eaf3de5e1dc868
kiIIer <michael.molchanov.2004@gmail.com>
2025-02-17 12:50:18 +0100

    information/lab-1/fixed-task1-6

--- information/lab-1/code/task1-6.lisp
+++ information/lab-1/code/task1-6.lisp
@@ -2,7 +2,7 @@
 (print (car (car (cdr '(a (b c) d)))))  ;; Результат: B
 
 ;; Варіант (b)
-(print ((lambda (a b) a) 13 (ignore-errors (/ 1 0))))  ;; Результат: 13
+(print (or 13 (/ 1 0)))  ;; Результат: 13
 
 ;; Варіант (c)
 (print (apply #'list* '(1 nil)))  ;; Результат: (1)
